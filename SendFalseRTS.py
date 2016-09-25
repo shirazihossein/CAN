9f3cf720e3734a3ce88fe7ca5c4a2ecff0f379fe
shirazee@gmail.com <shirazee@gmail.com>
2016-09-25 15:25:56 -0600

    First commit

--- SendFalseRTS.py
+++ SendFalseRTS.py
@@ -4,20 +4,22 @@ from Config import Config as cg
 
 
 can_name = "can1"
+strDes = "11"
 socket = SocketManager.SocketManager(can_name);
 
-strDes = "11"
-second_step = "18ec" + strDes + "00"
 
-third_step = "00EC00" +  strDes.upper()
-third_step_data  = "110701FFFFEBFE00"
+first_RTS = "18ec" + strDes + "00"
+
+false_RTS = "18EC00" +  strDes.upper()
+false_RTS_data  = "102C0001FFEBFE00"
+data_len = 8
 
 print ("#second step: waiting for engine to send a clera to send message")
 
 cf= socket.GetMessage(can_name)
-while (cf[0] != second_step):
+while (cf[0] != first_RTS):
 	cf= socket.GetMessage(can_name)
 
 
 #Third step: sending clear to send message for engine
-socket.SendMessage(third_step, data_len, third_step_data)
+socket.SendMessage(false_RTS, data_len, false_RTS_data)
